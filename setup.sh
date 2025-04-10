786d8e78e2239da8883c1c42aef6e43214162dcf
isaiah <ijshurtliff@gmail.com>
2025-04-10 19:56:59 +0000

    added files

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# safety
+set -Eeuo pipefail
+set +x
+
+# packages
+sudo apt install -y fish,starship,neofetch
+
+# fish setup
+echo /usr/bin/fish | sudo tee -a /etc/shells
+chsh -s /usr/bin/fish
+cp -r ./config ~/.config
+
+# nerd font for starship
+curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash -s -- --silent
+export PATH="$HOME/.local/bin:$PATH"
+getnf -i Hack
+
+
